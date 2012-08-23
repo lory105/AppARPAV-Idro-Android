15a16854f9aa55a300a16f84e92595b499882763
lory105 <giacomo.lory@gmail.com>
2012-08-23 10:00:08 +0200

    prima di inserimento google map

--- src/it/arpav/mobile/apparpav/main/MapSensorActivity.java
+++ src/it/arpav/mobile/apparpav/main/MapSensorActivity.java
@@ -1,17 +1,29 @@
 package it.arpav.mobile.apparpav.main;
 
+import net.londatiga.android.ActionItem;
+import net.londatiga.android.QuickAction;
+import net.londatiga.android.R;
 import it.arpav.mobile.apparpav.utils.Util;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MapSensorActivity extends Activity {
+	
+	//action id of button_menu
+	private static final int ID_MY_LOCATION     = 1;
+	private static final int ID_NEAREST_STATION = 2;
+	private static final int ID_ACTIVE_GPS 		= 3;
+	
 	private ProgressDialog pdToLoadStations = null;
 	
     @Override
@@ -41,7 +53,51 @@ public class MapSensorActivity extends Activity {
     
     
     public void updateDispaly(){
+		// ---------------------------------------------
+		ActionItem myLocationItem 	= new ActionItem(ID_MY_LOCATION, "Mia posizione", getResources().getDrawable(R.drawable.location));
+		ActionItem nearestItem   	= new ActionItem(ID_NEAREST_STATION, "Stazione piu vicina", getResources().getDrawable(R.drawable.location));
+        ActionItem activeGpsItem 	= new ActionItem(ID_ACTIVE_GPS, "Attiva GPS", getResources().getDrawable(R.drawable.gps));
+    	
+//		ActionItem myLocationItem 	= new ActionItem(ID_MY_LOCATION, "Next");
+//		ActionItem nearestItem   	= new ActionItem(ID_NEAREST_STATION, "Prev");
+//        ActionItem activeGpsItem 	= new ActionItem(ID_ACTIVE_GPS, "Find");
+    	
+        final QuickAction mQuickAction 	= new QuickAction(this );
+        
+		mQuickAction.addActionItem(myLocationItem);
+		mQuickAction.addActionItem(nearestItem);
+		mQuickAction.addActionItem(activeGpsItem);
 		
+		
+        
+        
+		//setup the action item click listener
+		mQuickAction.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
+			@Override
+			public void onItemClick(QuickAction quickAction, int pos, int actionId) {
+				
+				if (actionId == ID_MY_LOCATION) {
+					//Intent newintent = new Intent( getApplicationContext(), ConfActivity.class);
+					//newintent.putExtra("reload", true);
+					//startActivity(newintent);
+				} else if (actionId == ID_NEAREST_STATION ) {
+					Toast.makeText(getApplicationContext(), "I have no info this time", Toast.LENGTH_SHORT).show();
+				} else {
+					Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+					startActivity(intent);
+				}
+			}
+		});
+		
+		ImageButton buttonMenu = (ImageButton) findViewById(R.id.button_menu);
+	    buttonMenu.setOnClickListener(new View.OnClickListener() {
+		       @Override
+		       public void onClick(View v) {
+					mQuickAction.show(v);
+		       }
+		    });
+		// ---------------------------------------------
+        
     	 // btn_favorites is active
 		final Button btnFavorites = (Button) this.findViewById(R.id.btn_favorites);
 		btnFavorites.setBackgroundDrawable(this.getResources().getDrawable(R.drawable.bg_footer_reversed));
